7989a1c8643b5734861bc5f211b90f2033dccf93
longbowlu <zl9394@gmail.com>
2022-08-15 08:31:46 -0700

    comments and remove unwrap

--- crates/sui-json-rpc/src/api.rs
+++ crates/sui-json-rpc/src/api.rs
@@ -18,6 +18,12 @@ use sui_types::crypto::SignatureScheme;
 use sui_types::object::Owner;
 use sui_types::sui_serde::Base64;
 
+/// Maximum number of events returned in an event query.
+/// This is equivalent to EVENT_STORE_QUERY_MAX_LIMIT in `sui-storage` crate.
+/// To avoid unnecessary dependency on that crate, we have a reference here
+/// for document purposes.
+pub const EVENT_QUERY_MAX_LIMIT: usize = 100;
+
 #[open_rpc(namespace = "sui", tag = "Gateway Transaction Execution API")]
 #[rpc(server, client, namespace = "sui")]
 pub trait RpcGatewayApi {
@@ -349,7 +355,7 @@ pub trait EventReadApi {
         &self,
         /// digest of the transaction, as base-64 encoded string
         digest: TransactionDigest,
-        /// maximum size of the result, capped to 100
+        /// maximum size of the result, capped to EVENT_QUERY_MAX_LIMIT
         count: usize,
     ) -> RpcResult<Vec<SuiEventEnvelope>>;
 
@@ -361,7 +367,7 @@ pub trait EventReadApi {
         package: ObjectID,
         /// the module name
         module: String,
-        /// maximum size of the result, capped to 100
+        /// maximum size of the result, capped to EVENT_QUERY_MAX_LIMIT
         count: usize,
         /// the matching events' timestamp will be after the specified start time
         start_time: u64,
@@ -375,7 +381,7 @@ pub trait EventReadApi {
         &self,
         /// the event struct name type, e.g. `0x2::devnet_nft::MintNFTEvent` or `0x2::SUI::test_foo<address, vector<u8>>` with type params
         move_event_struct_name: String,
-        /// maximum size of the result, capped to 100
+        /// maximum size of the result, capped to EVENT_QUERY_MAX_LIMIT
         count: usize,
         /// the matching events' timestamp will be after the specified start time
         start_time: u64,
@@ -389,7 +395,7 @@ pub trait EventReadApi {
         &self,
         /// the sender's Sui address
         sender: SuiAddress,
-        /// maximum size of the result, capped to 100
+        /// maximum size of the result, capped to EVENT_QUERY_MAX_LIMIT
         count: usize,
         /// the matching events' timestamp will be after the specified start time
         start_time: u64,
@@ -403,7 +409,7 @@ pub trait EventReadApi {
         &self,
         /// the recipient
         recipient: Owner,
-        /// maximum size of the result, capped to 100
+        /// maximum size of the result, capped to EVENT_QUERY_MAX_LIMIT
         count: usize,
         /// the matching events' timestamp will be after the specified start time
         start_time: u64,
@@ -417,7 +423,7 @@ pub trait EventReadApi {
         &self,
         /// the object ID
         object: ObjectID,
-        /// maximum size of the result, capped to 100
+        /// maximum size of the result, capped to EVENT_QUERY_MAX_LIMIT
         count: usize,
         /// the matching events' timestamp will be after the specified start time
         start_time: u64,
